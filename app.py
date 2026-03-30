e55d524dce580706bff381036b1428e17fb2b191
Angelina <chertok.angelina.v@mail.ru>
2026-03-30 14:57:17 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+    print("add(5,3) =", add(5,3))
+    print("subtract(5,3) =", subtract(5,3))
+    print("multiply(5,3) =", multiply(5,3))
+    print("divide(6,2) =", divide(6,2))
